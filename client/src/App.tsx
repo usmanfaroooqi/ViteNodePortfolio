409e20322779cae120fdfd23b169395bbd08fac1
Usman Farooqi <hzusfq123@gmail.com>
2025-11-27 01:35:24 +0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,28 @@
-// In your App.js or App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import { Portfolio } from './components/Portfolio';
 import { PortfolioRepositories } from './components/PortfolioRepositories';
 import { PortfolioProjects } from './components/PortfolioProjects';
 import { PortfolioProjectDetail } from './components/PortfolioProjectDetail';
+// Import your other components
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Your existing routes */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
         
-        {/* ADD THESE PORTFOLIO ROUTES */}
+        {/* NEW: Separate portfolio routes */}
         <Route path="/portfolio" element={<PortfolioRepositories />} />
         <Route path="/portfolio/:repoId" element={<PortfolioProjects />} />
         <Route path="/portfolio/:repoId/project/:projectId" element={<PortfolioProjectDetail />} />
+        
+        {/* REMOVE or COMMENT OUT the old single portfolio route */}
+        {/* <Route path="/portfolio" element={<Portfolio />} /> */}
       </Routes>
     </Router>
   );
