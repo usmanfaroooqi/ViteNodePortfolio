b279a28f546bf19858495ea9628a266400592487
Usman Farooqi <hzusfq123@gmail.com>
2025-11-26 20:51:46 +0000

    Fix routes: use existing Home/About/Contact components and keep portfolio routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
 import { PortfolioRepositories } from './components/PortfolioRepositories';
 import { PortfolioProjects } from './components/PortfolioProjects';
 import { PortfolioProjectDetail } from './components/PortfolioProjectDetail';
-// Import your other components
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
-import ContactPage from './components/ContactPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Your existing routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         
         {/* NEW: Separate portfolio routes */}
         <Route path="/portfolio" element={<PortfolioRepositories />} />
